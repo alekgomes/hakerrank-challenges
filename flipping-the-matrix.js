ebce8e8a87bb5c2fcd8a62830229479991f58892
Lucas Álek <40683390+alekgomes@users.noreply.github.com>
2022-11-19 19:40:55 -0300

    Create flipping-the-matrix.js

---
+++ flipping-the-matrix.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'flippingMatrix' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+ */
+
+function flippingMatrix(matrix) {
+    // Write your code here
+        
+    const n = matrix.length/2
+    let total = 0
+    
+    for(let row = 0; row < n; row++) {
+        for(let col = 0; col < n; col++){
+            let max = Number.MIN_SAFE_INTEGER
+            max = Math.max(matrix[row][col],
+                            matrix[row][2 * n - col -1],
+                            matrix[2 * n - row - 1][col],
+                            matrix[2 * n - row - 1][2 * n - col -1]
+                            )            
+            total = total + max
+        }
+    }
+    return total
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        let matrix = Array(2 * n);
+
+        for (let i = 0; i < 2 * n; i++) {
+            matrix[i] = readLine().replace(/\s+$/g, '').split(' ').map(matrixTemp => parseInt(matrixTemp, 10));
+        }
+
+        const result = flippingMatrix(matrix);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
